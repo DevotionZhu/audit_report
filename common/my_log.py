b51888f4444ef584dcf2b8555bd900b4affc3007
wangmengmeng <wangmm_0165@163.com>
2019-06-04 14:48:10 +0800

    self.fh = logging.FileHandler("fail_log.txt", mode='w') 写入fail_log.txt时追加改为覆盖

--- common/my_log.py
+++ common/my_log.py
@@ -8,14 +8,14 @@ class Logger():
 
     def __init__(self):
         self.logger_obj = logging.getLogger()
-        self.fh = logging.FileHandler("fail_log.txt")  # 创建一个文件输出流
+        self.fh = logging.FileHandler("fail_log.txt", mode='w')  # 创建一个文件输出流
         self.fh.setLevel(logging.ERROR)  # 定义文件输出流的告警级别
         formater = logging.Formatter(
             '%(asctime)s - %(levelname)s %(pathname)s[line:%(lineno)d]: %(message)s')  # 定义日志输出格式
         self.fh.setFormatter(formater)  # 给Handler选择一个格式
         self.logger_obj.addHandler(self.fh)
 
-    def mylog(self,content):
+    def mylog(self, content):
         return self.logger_obj.error(content)
 
 
